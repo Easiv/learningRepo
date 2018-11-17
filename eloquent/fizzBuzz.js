5410f781170e7625313aed2874cb168465f3bffd
Easiv <easiv89@gmail.com>
2018-11-17 19:06:23 +0100

    chapter 2 exer 2 step 2

--- eloquent/fizzBuzz.js
+++ eloquent/fizzBuzz.js
@@ -1,14 +1,18 @@
 function fizzBuzz() {
   let numbers = []
   for(let i = 1; i <= 100; i++) {
-    if(i % 3 === 0) {
+    if(i % 5 === 0 && i % 3 === 0) {
+      console.log('FizzBuzz');
+      numbers.push('FizzBuzz');
+    } else if(i % 5 === 0 && i % 3 !== 0) {
+      console.log('Buzz');
+      numbers.push('Buzz');
+    } else if(i % 3 === 0) {
       console.log('Fizz');
       numbers.push('Fizz');
-    } else if(i % 5 === 0 && i % 3 !== 0) {
-      console.log('Buzz')
-      numbers.push('Buzz')
-    } else {
-      console.log(i) 
+    }
+    else {
+      console.log(i);
       numbers.push(i);
     }
   }
@@ -16,3 +20,4 @@ function fizzBuzz() {
 }
 
 module.exports = fizzBuzz;
+
