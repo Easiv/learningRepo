f4f253da386119bedc70f154afb32b41af1e4bb9
Easiv <easiv89@gmail.com>
2018-11-22 00:06:36 +0100

    remove console.log

--- eloquent/reverseArray.js
+++ eloquent/reverseArray.js
@@ -2,9 +2,7 @@ function reverseArray(arr) {
   let reversed = [];
   
   for(i=arr.length; i > 0; i--) {
-    reversed.push(arr[i])
-    console.log(reversed)
-    console.log(i)
+    reversed.push(arr[i - 1])
   }
   return reversed;
 }
